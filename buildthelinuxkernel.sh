6c2f01f74737d6d741b446f4ddb5504c90379a38
liammcdade <83595569+liammcdade@users.noreply.github.com>
2023-09-17 22:16:48 +0100

    Update buildthelinuxkernel.sh

--- buildthelinuxkernel.sh
+++ buildthelinuxkernel.sh
@@ -1,11 +1,9 @@
-#!/bin/bash/
-
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.5.3.tar.xz
 tar xvf linux-6.5.3.tar.xz
 sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
 cd linux-6.5.3
-make
 cp -v /boot/config-$(uname -r) .config
+make
 scripts/config --disable SYSTEM_TRUSTED_KEYS
 scripts/config --disable SYSTEM_REVOCATION_KEYS
 sudo make modules_install
