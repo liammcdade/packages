6c9e2107632b8d8b827d3f0fc17a72ad49b308b9
liammcdade <83595569+liammcdade@users.noreply.github.com>
2023-09-17 19:33:09 +0100

    Update build the linux kernel.sh

--- build the linux kernel.sh
+++ build the linux kernel.sh
@@ -1,7 +1,7 @@
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.10.11.tar.xz
-tar xvf linux-5.10.11.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.5.3.tar.xz
+tar xvf linux-6.5.3.tar.xz
 sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
-cd linux-6.0.7
+cd linux-6.5.3
 make
 cp -v /boot/config-$(uname -r) .config
 make menuconfig
@@ -9,5 +9,5 @@ scripts/config --disable SYSTEM_TRUSTED_KEYS
 scripts/config --disable SYSTEM_REVOCATION_KEYS
 sudo make modules_install
 sudo make install
-sudo update-initramfs -c -k 6.0.7
+sudo update-initramfs -c -k linux-6.5.3
 sudo update-grub
