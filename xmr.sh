e7faa637888c2a990ec3bda9e39f82e2ad0e0b11
liammcdade <83595569+liammcdade@users.noreply.github.com>
2023-10-02 14:28:05 +0100

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -7,5 +7,5 @@ cd xmrig
 sudo mkdir build
 cd build 
 sudo cmake ..
- sudo make
+sudo make
 ./xmrig -u 469BLJrSuanFAgyw1QnoMjV5CaZbARRNcHznox7v4seRSTK7GvKf1y2bZTRSzrQjrK8ikbe7woWYaLM39bNxJiWkD553YTG -p linux
